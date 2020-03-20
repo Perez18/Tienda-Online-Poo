6afc34030c59098f4ae05e3d229e4d93dba9fb3c
Anthony Perez <anperez@star.local>
2020-03-19 22:48:07 -0500

    Paramentros de configuraciones

--- config/parametros.php
+++ config/parametros.php
@@ -1,5 +1,5 @@
 <?php
 
-define("base_url","http://localhost/proyecto%20poo/");
+define("base_url","http://localhost/proyecto-poo/");
 define("controlador_default","productocontroller");
 define("action_default","index");
